b99c6aa47b38e511691f5de2616c001673f4957c
ChenSiLiang <284565035@qq.com>
2017-02-04 17:09:44 +0800

    turn screen on when running android test

--- boxing-impl/src/androidTest/java/com/bilibili/boxing_impl/view/PickerFragmentTest.java
+++ boxing-impl/src/androidTest/java/com/bilibili/boxing_impl/view/PickerFragmentTest.java
@@ -17,12 +17,16 @@
 
 package com.bilibili.boxing_impl.view;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.bilibili.boxing.model.entity.BaseMedia;
 import com.bilibili.boxing.model.entity.impl.ImageMedia;
@@ -50,7 +54,24 @@ public class PickerFragmentTest {
     public ActivityTestRule<TestBlankActivity> mRule = new ActivityTestRule<>(TestBlankActivity.class);
 
     @Before
-    public void setup() {
+    public void setup() throws Throwable {
+        // espresso need the screen on
+        final Activity activity = mRule.getActivity();
+        mRule.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                KeyguardManager km = (KeyguardManager) activity.getSystemService(Context.KEYGUARD_SERVICE);
+                KeyguardManager.KeyguardLock lock = km.newKeyguardLock(Context.KEYGUARD_SERVICE);
+                lock.disableKeyguard();
+
+                //turn the screen on
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+            }
+        });
     }
 
     @Test
